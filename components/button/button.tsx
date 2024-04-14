0f7f1786135babb0f59de8b65c861799aa5f2c25
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-14 22:54:23 +0900

    setting: storybook tailwind 적용

---
+++ components/button/button.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Button() {
+  // eslint-disable-next-line react/button-has-type
+  return <button className="bg-kakao hover:bg-black w-[100px] h-[50px] border border-black text-[25px]">버튼</button>;
+}
+
+export default Button;
