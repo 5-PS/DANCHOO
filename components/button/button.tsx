fee9060b4d34122472a5f336a82ca6352f2a4aef
minho <kasamin98@naver.com>
2024-04-16 00:55:51 +0900

    feat: 공용버튼 컴포넌트 추가

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,5 +1,28 @@
-function Button() {
-  return <button className="bg-kakao hover:bg-black w-[100px] h-[50px] border border-black text-[25px]">버튼</button>;
+'use client';
+
+import React, { ReactNode } from 'react';
+
+interface ButtonProps {
+  children: ReactNode;
+  background: string;
+  color: string;
+  onClick: any;
+}
+/**
+ * 가로값이 100%이므로 container로 감싸서 크기 조절을 해주세요, height는 48px고정
+ * @param background : tailwind 코드로입력(bg-primary)
+ * @param color : tailwind 코드로입력(text-primary)
+ * @param color : tailwind 코드로입력(text-primary)
+ * @returns
+ */
+function Button({ children, ...rest }: ButtonProps) {
+  const buttonClasses = `${rest.background} ${rest.color} ${rest.background === 'bg-white' ? 'border-primary border' : ''} font-bold hover:bg-orange-400 w-auto h-[48px]  text-[16px] rounded-md font-sans`;
+
+  return (
+    <button className={buttonClasses} onClick={rest.onClick}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
