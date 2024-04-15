51034186a223d915bc24d5f1621fffa836ae76fa
minho <kasamin98@naver.com>
2024-04-16 03:12:47 +0900

    comment: 공용버튼 주석 수정 및 타입지정

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -6,15 +6,16 @@ interface ButtonProps {
   children: ReactNode;
   background: string;
   color: string;
-  onClick: any;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 /**
  * 가로값이 100%이므로 container로 감싸서 크기 조절을 해주세요, height는 48px고정
- * @param background : tailwind 코드로입력(bg-primary)
- * @param color : tailwind 코드로입력(text-primary)
- * @param color : tailwind 코드로입력(text-primary)
- * @returns
+ * @param background tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
+ * @param color tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
+ * @param onClick 핸들러 함수
  */
+
+// TODO: 폰트설정
 function Button({ children, ...rest }: ButtonProps) {
   const buttonClasses = `${rest.background} ${rest.color} 
   ${rest.background === 'bg-white' ? 'border-primary border hover:bg-gray-10' : ''} 
