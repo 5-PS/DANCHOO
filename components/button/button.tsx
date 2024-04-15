d192207a2d07f82efcab5fc548b3f46c46682453
minho <kasamin98@naver.com>
2024-04-16 03:35:13 +0900

    refector: 공용버튼 프롭을 2개만 받도록 수정

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -5,22 +5,20 @@ import React, { ReactNode } from 'react';
 interface ButtonProps {
   children: ReactNode;
   background: string;
-  color: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 /**
  * 가로값이 100%이므로 container로 감싸서 크기 조절을 해주세요, height는 48px고정
  * @param background tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
- * @param color tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
  * @param onClick 핸들러 함수
  */
 
 // TODO: 폰트설정
 function Button({ children, ...rest }: ButtonProps) {
-  const buttonClasses = `${rest.background} ${rest.color} 
-  ${rest.background === 'bg-white' ? 'border-primary border hover:bg-gray-10' : ''} 
-  ${rest.background === 'bg-gray-40' ? 'hover:bg-gray-50' : ''} 
-  ${rest.background === 'bg-primary' ? 'hover:bg-primary-hover' : ''}
+  const buttonClasses = `${rest.background}
+  ${rest.background === 'bg-white' ? 'border-primary border hover:bg-gray-10 text-primary' : ''} 
+  ${rest.background === 'bg-gray-40' ? 'hover:bg-gray-50 text-white ' : ''} 
+  ${rest.background === 'bg-primary' ? 'hover:bg-primary-hover text-white ' : ''}
   font-bold active:scale-95 w-full h-[48px] shadow text-[16px] rounded-md font-sans`;
 
   return (
