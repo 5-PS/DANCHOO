b0e0d00e4423e58fdaf5ab84346336a3beaf121f
namju <skawn1228@gmail.com>
2024-04-17 02:25:48 +0900

    feat: input 에러 시 테두리 색 변경

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -10,20 +10,17 @@ interface InputProps extends ComponentPropsWithoutRef<'input'> {
 }
 
 export default forwardRef<HTMLInputElement, InputProps>(
-  ({ label, type, errorMessage, placeholder, ...restProps }, ref) => (
-    <div className="flex flex-col w-full gap-2">
-      <label htmlFor="id" className="text-black leading-[26px]">
-        {label}
-      </label>
-      <input
-        ref={ref}
-        id="id"
-        type={type}
-        placeholder={placeholder}
-        {...restProps}
-        className="px-5 py-4 bg-white border border-solid rounded-md outline-none border-gray-30"
-      />
-      {errorMessage && <span className="text-xs text-red-40">{errorMessage}</span>}
-    </div>
-  ),
+  ({ label, type, errorMessage, placeholder, ...restProps }, ref) => {
+    const inputClassName = `px-5 py-4 bg-white border border-solid rounded-md outline-none ${errorMessage ? 'border-[#FF4040]' : 'border-gray-30'}`;
+
+    return (
+      <div className="flex flex-col w-full gap-2">
+        <label htmlFor="id" className="text-black leading-[26px]">
+          {label}
+        </label>
+        <input ref={ref} id="id" type={type} placeholder={placeholder} className={inputClassName} {...restProps} />
+        {errorMessage && <span className="text-xs text-red-500">{errorMessage}</span>}
+      </div>
+    );
+  },
 );
