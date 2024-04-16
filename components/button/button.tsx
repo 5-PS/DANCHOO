5f7c6be9ce65467f6c0f5a8a8885b05ad7452705
minho <kasamin98@naver.com>
2024-04-16 12:44:26 +0900

    feat: 공용버튼 버튼 사이트 받아서 사이즈마다 스타일링 다르게 설정

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -5,11 +5,13 @@ import React, { ReactNode } from 'react';
 interface ButtonProps {
   children: ReactNode;
   background: string;
+  fontSize: number;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 /**
  * 가로값이 100%이므로 container로 감싸서 크기 조절을 해주세요, height는 48px고정
  * @param background tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
+ * @param fontSize 16,14,12 중 입력
  * @param onClick 핸들러 함수
  */
 
@@ -19,7 +21,10 @@ function Button({ children, ...rest }: ButtonProps) {
   ${rest.background === 'bg-white' ? 'border-primary border hover:bg-gray-10 text-primary' : ''} 
   ${rest.background === 'bg-gray-40' ? 'hover:bg-gray-50 text-white ' : ''} 
   ${rest.background === 'bg-primary' ? 'hover:bg-primary-hover text-white ' : ''}
-  font-bold active:scale-95 w-full h-[48px] shadow text-[16px] rounded-md font-sans`;
+  ${rest.fontSize === 16 ? 'text-base font-bold leading-5' : ''} 
+  ${rest.fontSize === 14 ? 'text-sm font-bold' : ''} 
+  ${rest.fontSize === 12 ? 'text-xs font-normal leading-4' : ''} 
+  active:scale-95 w-full h-[48px] shadow text-[16px] rounded-md font-sans`;
 
   return (
     <button className={buttonClasses} onClick={rest.onClick}>
