99ad60c09fcb892fe691afcca273cade837124b0
namju <skawn1228@gmail.com>
2024-04-16 17:45:59 +0900

    feat: SigninForm 컴포넌트 생성

--- app/signin/_components/signinForm.tsx
+++ app/signin/_components/signinForm.tsx
@@ -2,8 +2,16 @@
 
 import { FieldValues, useForm } from 'react-hook-form';
 
+import Input from '@/components/input/input';
+
+const EMAIL_REGEX = /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[a-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/;
+
 export default function SigninForm() {
-  const { handleSubmit } = useForm<FieldValues>({ mode: 'all' });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({ mode: 'all' });
 
   const onSubmit = () => {
     // TODO: submit 함수 작성
@@ -11,7 +19,31 @@ export default function SigninForm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col w-[300px] gap-7">{/* TODO: input 컴포넌트 추가 */}</div>
+      <div className="flex flex-col w-[300px] gap-7">
+        {/* FIXME: w-[300px] gap-7 스타일 적용 안됨 */}
+        <Input
+          label="이메일"
+          type="text"
+          errorMessage={errors.email?.message}
+          placeholder="이메일을 입력해주세요."
+          {...register('email', {
+            required: '이메일을 입력해주세요.',
+            pattern: {
+              value: EMAIL_REGEX,
+              message: '올바른 이메일 주소가 아닙니다.',
+            },
+          })}
+        />
+        <Input
+          label="비밀번호"
+          type="password"
+          errorMessage={errors.password?.message}
+          placeholder="비밀번호를 입력해주세요."
+          {...register('password', {
+            required: '비밀번호를 입력해주세요.',
+          })}
+        />
+      </div>
       {/* TODO: button 컴포넌트 추가 */}
     </form>
   );
