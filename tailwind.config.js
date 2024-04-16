c4e23d151902bcb596748d6c95984071b2030580
namju <skawn1228@gmail.com>
2024-04-16 20:46:12 +0900

    setting: `app` 폴더 내에서도 tailwind 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./components/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.stories.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.stories.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
     extend: {
       colors: {
