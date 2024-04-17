2c628d72ba85e0ebfa3bd7780412148238f29941
minho <kasamin98@naver.com>
2024-04-18 00:31:09 +0900

    feat: 공용버튼의 높이조절 기능 추가 및 코드 리팩토링

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -6,44 +6,43 @@ import React, { ReactNode } from 'react';
  * @todo {...rest}끄는 옵션 추가 후 onClick 대신 ...rest로 받고 extends 리액트 이벤트
  * @todo 폰트설정
  */
-interface ButtonProps {
+interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   background: string;
   fontSize: number;
   width: number;
-  onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 /**
  * 가로값이 100%이므로 container로 감싸서 크기 조절을 해주세요, height는 48px고정
  * 버튼 disabled 상태가 되게 하려면 background 색상만 br-gray-40으로 바꿔주시면 됩니다.
+ * @param children 버튼에 줄 값입니다.
  * @param background tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
  * @param fontSize 16,14,12 중 입력
+ * @param width 48,37,32 중 입력
  * @param onClick 핸들러 함수
  */
-function Button({ children, ...rest }: ButtonProps) {
-  const buttonClasses = `${rest.background}
-  ${rest.background === 'bg-white' ? 'border-primary border hover:bg-gray-10 text-primary active:scale-95' : ''} 
-  ${rest.background === 'bg-gray-40' ? 'text-white ' : ''} 
-  ${rest.background === 'bg-primary' ? 'hover:bg-primary-hover text-white active:scale-95 ' : ''}
-  ${rest.fontSize === 16 ? 'text-base font-bold leading-5' : ''} 
-  ${rest.fontSize === 14 ? 'text-sm font-bold' : ''} 
-  ${rest.fontSize === 12 ? 'text-xs font-normal leading-4' : ''} 
-  ${rest.width === 48 ? 'h-[48px]' : ''}
-  ${rest.width === 37 ? 'h-[37px]' : ''}
-  ${rest.width === 32 ? 'h-[32px]' : ''}
+function Button({ children, background, fontSize, width, ...rest }: ButtonProps) {
+  const buttonClasses = `${background}
+  ${background === 'bg-white' ? 'border-primary border hover:bg-gray-10 text-primary active:scale-95' : ''} 
+  ${background === 'bg-gray-40' ? 'text-white ' : ''} 
+  ${background === 'bg-primary' ? 'hover:bg-primary-hover text-white active:scale-95 ' : ''}
+  ${fontSize === 16 ? 'text-base font-bold leading-5' : ''} 
+  ${fontSize === 14 ? 'text-sm font-bold' : ''} 
+  ${fontSize === 12 ? 'text-xs font-normal leading-4' : ''} 
+  ${width === 48 ? 'h-[48px]' : ''}
+  ${width === 37 ? 'h-[37px]' : ''}
+  ${width === 32 ? 'h-[32px]' : ''}
   w-full  shadow text-[16px] rounded-md font-sans`;
 
   function isGray() {
-    if (rest.background === 'bg-gray-40') {
+    if (background === 'bg-gray-40') {
       return true;
     }
     return false;
   }
-  /**
-   * @todo onClick삭제
-   */
+
   return (
-    <button className={buttonClasses} onClick={rest.onClick} disabled={isGray()}>
+    <button className={buttonClasses} disabled={isGray()} {...rest}>
       {children}
     </button>
   );
