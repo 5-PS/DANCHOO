4b470530bdefccdaafc3015122eb60feb14678bb
황준용 <wnsdyd3333@gmail.com>
2024-04-18 19:08:50 +0900

    design: pc버전 gap 수치 수정

--- components/gnb/gnb.tsx
+++ components/gnb/gnb.tsx
@@ -60,7 +60,7 @@ function Gnb() {
   const userType: string = 'employee';
   return (
     <header className="sticky top-0 z-10 px-5 py-3 m-auto bg-white md:p-8 w-full md:max-w-[964px]">
-      <nav className="flex flex-wrap items-center gap-4 md:relative">
+      <nav className="flex flex-wrap items-center gap-4 md:relative md:gap-10">
         <Link href="/">
           <Image src="/icons/logo.svg" width={112} height={40} alt="로고" />
         </Link>
