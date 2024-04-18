e413a04fc9284f5b3d46c18ad564d841c3b724a2
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-18 23:08:39 +0900

    design: 모바일 우선 디자인 적용

--- app/(service)/my-store/my-recruits/[recruitId]/_components/recruitsCard.tsx
+++ app/(service)/my-store/my-recruits/[recruitId]/_components/recruitsCard.tsx
@@ -1,25 +1,27 @@
 import Button from '@/components/button/button';
 
+// TODO: 시급비교 라벨 따로빼기, 주소, 시간 아이콘 넣기
+
 function RecuitsCard() {
   return (
     <>
-      <div className="flex w-full gap-8 p-6 mb-6 border rounded-xl border-gray-20 max-xl:flex-col max-xl:gap-4 max-md:p-5 max-md:gap-3">
-        <div className="w-full max-xl:h-[361px] max-md:h-[178px]">
-          <div className="h-full rounded-xl bg-fuchsia-600">사진</div>
+      <div className="flex flex-col w-full gap-3 p-5 mb-3 border rounded-xl border-gray-20 md:p-6 md:mb-6 md:gap-4 xl:flex-row xl:gap-8">
+        <div className="w-full h-[178px] md:h-[361px] xl:h-auto">
+          <div className="h-full rounded-xl bg-red-20" />
         </div>
-        <div className="flex flex-col gap-3 pt-4 max-xl:pt-0 max-md:gap-2">
+        <div className="flex flex-col gap-2 xl:pt-4 md:gap-3">
           <div>
-            <span className="inline-block mb-2 font-bold text-primary max-md:text-[14px]">시급</span>
-            <div className="font-bold text-[28px] flex gap-2 items-center max-md:text-[24px]">
+            <span className="inline-block mb-2 font-bold text-primary text-[14px] md:text-[16px]">시급</span>
+            <div className="font-bold text-[24px] flex gap-2 items-center md:text-[28px]">
               15,000원
               <span className=" p-3 rounded-[20px] text-white bg-primary text-[14px] h-[36px] max-md:text-[12px] max-md:p-2 max-md:h-auto">
                 기존 시급보다 50% 업
               </span>
             </div>
           </div>
-          <div className="text-gray-50 max-md:text-[14px]">asdasda</div>
-          <div className="text-gray-50 max-md:text-[14px]">asdasda</div>
-          <p className="leading-[26px] max-md:text-[14px]">
+          <div className="text-gray-50 text-[14px] md:text-[16px]">asdasda</div>
+          <div className="text-gray-50 text-[14px] md:text-[16px]">asdasda</div>
+          <p className="leading-[26px] text-[14px] md:text-[16px]">
             알바하기 편한 너구리네 라면집! 라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다.
           </p>
           <Button background="bg-white" fontSize={16}>
@@ -27,7 +29,7 @@ function RecuitsCard() {
           </Button>
         </div>
       </div>
-      <div className="flex flex-col w-full gap-3 p-8 rounded-xl bg-red-10 max-md:text-[14px] max-md:gap-2 max-md:p-5">
+      <div className="flex flex-col w-full gap-2 p-5 rounded-xl bg-red-10 text-[14px] md:text-[16px] md:p-8 md:gap-3">
         <h2 className="font-bold">공고 설명</h2>
         <p className="text-pretty">
           기존 알바 친구가 그만둬서 새로운 친구를 구했는데, 그 사이에 하루가 비네요. 급해서 시급도 높였고 그렇게 바쁜
--- app/(service)/my-store/my-recruits/[recruitId]/page.tsx
+++ app/(service)/my-store/my-recruits/[recruitId]/page.tsx
@@ -8,15 +8,15 @@ function MyRecruitDetailPage({ searchParams }) {
     <>
       <div className="w-full h-[70px] bg-black text-white">헤더부분</div>
       <main>
-        <section className="py-[60px] max-w-[964px] m-auto max-xl:max-w-[680px] max-md:py-10 max-md:max-w-full max-md:px-3">
-          <div className="mb-6 ">
-            <span className="inline-block mb-2 font-bold text-primary max-md:text-[14px]">식당</span>
-            <h1 className="text-[28px] font-bold max-md:text-[20px]">도토리 식당</h1>
+        <section className="my-recuits-detail-section">
+          <div className="mb-4 md:mb-6">
+            <span className="text-[14px] inline-block mb-2 font-bold text-primary md:text-[16px]">식당</span>
+            <h1 className="text-[20px] font-bold md:text-[28px]">도토리 식당</h1>
           </div>
           <RecuitsCard />
         </section>
-        <section className="py-[60px] max-w-[964px] m-auto max-xl:max-w-[680px] max-md:py-10 max-md:max-w-full max-md:px-3">
-          <h1 className="text-[28px] font-bold mb-8">신청자 목록</h1>
+        <section className="my-recuits-detail-section">
+          <h1 className="text-[20px] font-bold mb-4 md:text-[28px] md:mb-8 ">신청자 목록</h1>
           <Table query={searchParams} />
         </section>
       </main>
