b24ce58400348b29da066a94444fd5a194e11727
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-19 01:47:26 +0900

    feat: page 생성

---
+++ app/(service)/my-store/edit/page.tsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import MyStoreForm from './_components/myStoreForm';
+
+export default function MystoreEditFormPage() {
+  return (
+    <div>
+      <div className="h-[70px] bg-red-20">헤더</div>
+      <div className="w-full max-w-[964px] flex flex-col gap-8 m-auto py-[60px]">
+        <div className="flex items-center justify-between">
+          <h1 className="text-[28px] font-bold">가게 정보</h1>
+          <Link href="/">
+            <Image src="/icons/close-modal-icon.svg" width={32} height={32} alt="이전버튼" />
+          </Link>
+        </div>
+        <MyStoreForm />
+      </div>
+    </div>
+  );
+}
