91e12ce1caeac1ecab986fea93d80981bb30808f
minho <kasamin98@naver.com>
2024-04-19 03:25:36 +0900

    refactor: 공용버튼 isGray()함수 삭제

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -3,7 +3,6 @@
 import React, { ReactNode } from 'react';
 
 /**
- * @todo {...rest}끄는 옵션 추가 후 onClick 대신 ...rest로 받고 extends 리액트 이벤트
  * @todo 폰트설정
  */
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
@@ -15,11 +14,11 @@ interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
 /**
  * 가로값이 100%이므로 container로 감싸서 크기 조절을 해주세요, height는 48px고정
  * 버튼 disabled 상태가 되게 하려면 background 색상만 br-gray-40으로 바꿔주시면 됩니다.
+ * 핸들러 함수 선택적 등록
  * @param children 버튼에 줄 값입니다.
  * @param background tailwind 코드로입력 택1(bg-primary, bg-white, bg-gray-40)
  * @param fontSize 16,14,12 중 입력
  * @param width 48,37,32 중 입력
- * @param onClick 핸들러 함수
  */
 function Button({ children, background, fontSize, width, ...rest }: ButtonProps) {
   const buttonClasses = `${background}
@@ -34,15 +33,8 @@ function Button({ children, background, fontSize, width, ...rest }: ButtonProps)
   ${width === 32 ? 'h-[32px]' : ''}
   w-full  shadow text-[16px] rounded-md font-sans`;
 
-  function isGray() {
-    if (background === 'bg-gray-40') {
-      return true;
-    }
-    return false;
-  }
-
   return (
-    <button className={buttonClasses} disabled={isGray()} {...rest}>
+    <button className={buttonClasses} disabled={background === 'bg-gray-40'} {...rest}>
       {children}
     </button>
   );
