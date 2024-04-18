7bf1d28d23e2df680168f6e1d67a285b306ee47a
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-18 16:10:01 +0900

    feat: table 컴포넌트 결합

--- app/(service)/my-store/my-recruits/[recruitId]/page.tsx
+++ app/(service)/my-store/my-recruits/[recruitId]/page.tsx
@@ -1,18 +1,26 @@
+import Footer from '@/components/footer/footer';
+import Table from '@/components/table/table';
+
 import RecuitsCard from './_components/recruitsCard';
 
-function MyRecruitDetailPage() {
+function MyRecruitDetailPage({ searchParams }) {
   return (
     <>
       <div className="w-full h-[70px] bg-black text-white">헤더부분</div>
       <main>
-        <section className="py-[60px] max-w-[964px] m-auto ">
+        <section className="py-[60px] max-w-[964px] m-auto">
           <div className="mb-6 ">
-            <span className="inline-block mb-2 font-bold">식당</span>
+            <span className="inline-block mb-2 font-bold text-primary">식당</span>
             <h1 className="text-[28px] font-bold">도토리 식당</h1>
           </div>
           <RecuitsCard />
         </section>
+        <section className="py-[60px] max-w-[964px] m-auto">
+          <h1 className="text-[28px] font-bold mb-8">신청자 목록</h1>
+          <Table query={searchParams} />
+        </section>
       </main>
+      <Footer />
     </>
   );
 }
