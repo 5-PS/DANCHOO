8df72b434eeba719297287a532b95bb1b5dfc7e0
황준용 <wnsdyd3333@gmail.com>
2024-04-18 17:30:41 +0900

    feat: gnb 컴포넌트 구현

--- components/gnb/gnb.tsx
+++ components/gnb/gnb.tsx
@@ -1,59 +1,86 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-function SearchBar() {
-  return (
-    <div className="flex justify-start items-center gap-[10px] p-[10px] bg-gray-10 rounded-xl w-[344px] lg:w-[450px] max-[768px]:search">
-      <Image src="/icons/search.svg" width={20} height={20} alt="돋보기" />
-      <input
-        className="w-full outline-none bg-gray-10 placeholder:text-gray-40 placeholder:text-sm"
-        type="text"
-        placeholder="가게 이름으로 찾아보세요"
-      />
-    </div>
-  );
+import NotificationBtn from './notificationBtn';
+import SearchBar from './searchBar';
+
+// 더미데이터
+const alertList = {
+  limit: 5, // 알림 개수
+  items: [
+    {
+      item: {
+        id: '1',
+        createdAt: '2024-04-18T08:00:00Z',
+        result: 'accepted', // 또는 'rejected'
+        shop: {
+          item: {
+            id: '1',
+            name: 'Shop A',
+            imageUrl: 'https://example.com/shop_a_image.jpg',
+          },
+        },
+        notice: {
+          item: {
+            startsAt: '2024-04-18T09:00:00Z',
+            workhour: 8,
+          },
+        },
+      },
+    },
+    {
+      item: {
+        id: '2',
+        createdAt: '2024-04-17T12:00:00Z',
+        result: 'rejected', // 또는 'accepted'
+        shop: {
+          item: {
+            id: '2',
+            name: 'Shop B',
+            imageUrl: 'https://example.com/shop_b_image.jpg',
+          },
+        },
+        notice: {
+          item: {
+            startsAt: '2024-04-17T13:00:00Z',
+            workhour: 6,
+          },
+        },
+      },
+    },
+  ],
+};
+function UserTypeLink({ userType }: { userType: string }) {
+  return userType === 'employee' ? <Link href="/my-profile">내 프로필</Link> : <Link href="/my-store">내 가게</Link>;
 }
 
-function Authenticated({ userType }: { userType: string }) {
+// TODO: 로그아웃 로직 만들어야 합니다
+function Gnb() {
+  const auth: boolean = true;
+  const userType: string = 'employee';
   return (
-    <div className="flex items-center justify-start gap-4 font-bold text-black max-[768px]:buttons">
-      {userType === 'employer' && <Link href="/myshop">내가게</Link>}
-      {userType === 'employee' && <Link href="/profile">내 프로필</Link>}
-      <Link href="/logout">로그아웃</Link>
-      <button>
-        <Image src="/icons/notification_inactive.svg" width={20} height={20} alt="알림" />
-      </button>
-    </div>
-  );
-}
-
-function UnAuthenticated() {
-  return (
-    <div className="flex items-center justify-start gap-10 font-bold text-black max-[768px]:buttons">
-      <Link href="/signin">로그인</Link>
-      <Link href="/signup">회원가입</Link>
-    </div>
-  );
-}
-
-interface GnbProps {
-  isAuthenticated: boolean;
-  userType: string;
-}
-// FIXME: 로그인 유저 상태관리 만들면 좀 많이 손 봐야함
-function Gnb({ isAuthenticated, userType }: GnbProps) {
-  return (
-    <nav className="sticky top-0 z-10 px-8 py-4 bg-white">
-      <div className="flex items-center justify-between max-w-5xl m-auto max-[768px]:gnb">
-        <Link className="max-[768px]:logo" href="/">
+    <header className="sticky top-0 z-10 px-5 py-3 m-auto bg-white md:p-8 w-full xl:w-[964px]">
+      <nav className="flex flex-wrap items-center gap-4 md:relative">
+        <Link href="/">
           <Image src="/icons/logo.svg" width={112} height={40} alt="로고" />
         </Link>
-
+        <div className="flex items-center gap-5 ml-auto font-bold text-black md:order-1">
+          {auth ? (
+            <>
+              <UserTypeLink userType={userType} />
+              <button>로그아웃</button>
+              <NotificationBtn alertList={alertList} />
+            </>
+          ) : (
+            <>
+              <Link href="/signin">로그인</Link>
+              <Link href="/signup">회원가입</Link>
+            </>
+          )}
+        </div>
         <SearchBar />
-
-        {isAuthenticated ? <Authenticated userType={userType} /> : <UnAuthenticated />}
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 }
 
