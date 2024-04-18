0118f9ffa13ac7be10b6ef2e3378614b9b4b0eac
namju <skawn1228@gmail.com>
2024-04-18 19:06:30 +0900

    feat: input 내 단위 텍스트 추가

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -7,18 +7,20 @@ interface InputProps extends ComponentPropsWithoutRef<'input'> {
   type: string;
   errorMessage: string | undefined;
   placeholder: string;
+  rightText: string | undefined;
 }
 
 export default forwardRef<HTMLInputElement, InputProps>(
-  ({ label, type, errorMessage, placeholder, ...restProps }, ref) => {
+  ({ label, type, errorMessage, placeholder, rightText, ...restProps }, ref) => {
     const inputClassName = `px-5 py-4 bg-white border border-solid rounded-md outline-none ${errorMessage ? 'border-[#FF4040]' : 'border-gray-30'}`;
 
     return (
-      <div className="flex flex-col w-full gap-2">
+      <div className="relative flex flex-col w-full gap-2">
         <label htmlFor="id" className="text-black leading-[26px]">
           {label}
         </label>
         <input ref={ref} id="id" type={type} placeholder={placeholder} className={inputClassName} {...restProps} />
+        {rightText && <span className=". absolute right-5 top-[50px]">{rightText}</span>}
         {errorMessage && <span className="text-xs text-red-500">{errorMessage}</span>}
       </div>
     );
