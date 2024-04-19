02f67e24aa01d7967931a95e2632f007cbe08701
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-19 16:12:49 +0900

    feat: react-hook-form 적용

--- app/(service)/my-store/edit/_components/myStoreForm.tsx
+++ app/(service)/my-store/edit/_components/myStoreForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
 
 import Image from 'next/image';
 
@@ -12,6 +13,11 @@ import SelectInput from '@/components/input/selectInput';
 // 3. SelectInput의 옵션 메뉴 position absolute 주기
 
 export default function MyStoreForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({ mode: 'all' });
   const [imageSrc, setImageSrc] = useState<any>(null);
 
   const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -24,38 +30,69 @@ export default function MyStoreForm() {
       };
     }
   };
-  console.log(imageSrc);
+  const onSubmit = () => {
+    // TODO: submit함수 구현
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
       <div className="flex flex-col gap-6 mb-8">
         <div className="flex gap-5">
-          <Input label="가게 이름*" />
+          <Input
+            label="가게 이름*"
+            type="text"
+            errorMessage={errors.storeName?.message}
+            placeholder="가게 이름을 적어주세요."
+            {...register('storeName', {
+              required: '가게 이름을 적어주세요',
+            })}
+          />
           <SelectInput />
         </div>
         <div className="flex gap-5">
           <SelectInput />
-          <Input label="상세 주소*" />
+          <Input
+            label="상세 주소*"
+            type="text"
+            errorMessage={errors.detailAddress?.message}
+            placeholder="주소를 입력해 주세요."
+            {...register('detailAddress', {
+              required: '상세 주소를 입력해주세요',
+            })}
+          />
         </div>
         <div className="w-[472px]">
           <Input label="기본 시급*" />
         </div>
-        <div className="w-[483px]">
+        <div className="w-[472px]">
           <p className="mb-2">가게 이미지</p>
           <label
             htmlFor="image-input"
-            className="flex flex-col gap-3 font-bold text-gray-40 w-full h-[276px] bg-gray-10 border border-gray-30 rounded-xl justify-center items-center cursor-pointer relative overflow-hidden"
+            className={`flex flex-col gap-3 font-bold text-gray-40 w-full h-[276px] bg-gray-10 border  ${!imageSrc && 'border-gray-20 '}  rounded-xl justify-center items-center cursor-pointer relative overflow-hidden`}
           >
             <div className="inline-flex flex-col items-center gap-3">
-              <Image src="/icons/image-add-icon.svg" width={32} height={32} />
+              <Image src="/icons/image-add-icon.svg" width={32} height={32} alt="사진 추가 아이콘" />
               이미지 선택
-              <input type="file" id="image-input" className="invisible w-0 h-0" onChange={handleUploadImage} />
+              <input
+                type="file"
+                id="image-input"
+                className="invisible w-0 h-0"
+                {...register('imageFile', {
+                  onChange: handleUploadImage,
+                })}
+              />
             </div>
             {imageSrc && <Image src={imageSrc} fill alt="미리보기 이미지" />}
           </label>
         </div>
         <div>
-          <p className="mb-2">가게 설명</p>
-          <textarea className="w-full h-[153px] rounded-[6px] px-5 py-4 border border-gray-30" />
+          <label htmlFor="store-description">
+            <p className="mb-2">가게 설명 </p>
+            <textarea
+              id="store-description"
+              className="w-full h-[153px] rounded-[6px] px-5 py-4 border border-gray-30"
+              {...register('storeDescription')}
+            />
+          </label>
         </div>
       </div>
       <div className="flex justify-center ">
--- app/(service)/my-store/edit/page.tsx
+++ app/(service)/my-store/edit/page.tsx
@@ -10,7 +10,7 @@ export default function MystoreEditFormPage() {
       <div className="w-full max-w-[964px] flex flex-col gap-8 m-auto py-[60px]">
         <div className="flex items-center justify-between">
           <h1 className="text-[28px] font-bold">가게 정보</h1>
-          <Link href="/">
+          <Link href="/my-store">
             <Image src="/icons/close-modal-icon.svg" width={32} height={32} alt="이전버튼" />
           </Link>
         </div>
