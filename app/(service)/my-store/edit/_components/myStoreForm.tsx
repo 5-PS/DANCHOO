e923c2fd93030d55ebed8c8c2b9b809efbbdcb95
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-19 14:13:48 +0900

    feat: 이미지업로드 미리보기 기능 구현

--- app/(service)/my-store/edit/_components/myStoreForm.tsx
+++ app/(service)/my-store/edit/_components/myStoreForm.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import { useState } from 'react';
+
+import Image from 'next/image';
+
 import Button from '@/components/button/button';
 import Input from '@/components/input/input';
 import SelectInput from '@/components/input/selectInput';
@@ -8,6 +12,19 @@ import SelectInput from '@/components/input/selectInput';
 // 3. SelectInput의 옵션 메뉴 position absolute 주기
 
 export default function MyStoreForm() {
+  const [imageSrc, setImageSrc] = useState<any>(null);
+
+  const handleUploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setImageSrc(reader.result);
+      };
+    }
+  };
+  console.log(imageSrc);
   return (
     <form>
       <div className="flex flex-col gap-6 mb-8">
@@ -24,7 +41,17 @@ export default function MyStoreForm() {
         </div>
         <div className="w-[483px]">
           <p className="mb-2">가게 이미지</p>
-          <div className="w-full h-[276px] bg-red-20 rounded-xl" />
+          <label
+            htmlFor="image-input"
+            className="flex flex-col gap-3 font-bold text-gray-40 w-full h-[276px] bg-gray-10 border border-gray-30 rounded-xl justify-center items-center cursor-pointer relative overflow-hidden"
+          >
+            <div className="inline-flex flex-col items-center gap-3">
+              <Image src="/icons/image-add-icon.svg" width={32} height={32} />
+              이미지 선택
+              <input type="file" id="image-input" className="invisible w-0 h-0" onChange={handleUploadImage} />
+            </div>
+            {imageSrc && <Image src={imageSrc} fill alt="미리보기 이미지" />}
+          </label>
         </div>
         <div>
           <p className="mb-2">가게 설명</p>
