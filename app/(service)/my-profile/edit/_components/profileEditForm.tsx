8fcb877fb059ba5abcc294e1c33e5610e0ce93fd
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-20 21:47:42 +0900

    design: 폼페이지 1차 반응형 디자인 완성

--- app/(service)/my-profile/edit/_components/profileEditForm.tsx
+++ app/(service)/my-profile/edit/_components/profileEditForm.tsx
@@ -7,21 +7,23 @@ import SelectInput from '@/components/input/selectInput';
 export default function ProfileEditForm() {
   return (
     <form>
-      <div className="flex flex-col gap-6 mb-8">
-        <div className="flex gap-5">
+      <div className="flex flex-col gap-6 mb-6 md:gap-8">
+        <div className="flex flex-col w-full gap-5 md:flex-row md:flex-wrap xl:flex-nowrap">
           <Input label="이름*" />
           <Input label="연락처*" />
           <SelectInput />
         </div>
-        <label htmlFor="introduction">소개</label>
-        <textarea
-          className="w-full px-5 py-4 border rounded-[5px] border-gray-30 h-[153px]"
-          placeholder="소개를 입력해주세요"
-          id="introduction"
-        />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="introduction">소개</label>
+          <textarea
+            className="w-full px-5 py-4 border rounded-[5px] border-gray-30 h-[153px]"
+            placeholder="소개를 입력해주세요"
+            id="introduction"
+          />
+        </div>
       </div>
       <div className="flex items-center justify-center">
-        <div className="w-[312px]">
+        <div className="w-full md:w-[312px]">
           <Button background="bg-primary" fontSize={16} height={48}>
             등록하기
           </Button>
