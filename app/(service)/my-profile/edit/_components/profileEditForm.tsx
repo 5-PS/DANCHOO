618b0d3480c71e853673a74e5066bd339acbfcd1
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-21 23:19:58 +0900

    fix: selectInput 리스트 값 연결

--- app/(service)/my-profile/edit/_components/profileEditForm.tsx
+++ app/(service)/my-profile/edit/_components/profileEditForm.tsx
@@ -6,6 +6,33 @@ import Button from '@/components/button/button';
 import Input from '@/components/input/input';
 import SelectInput from '@/components/input/selectInput';
 
+const ADDRESS_LIST = [
+  { id: 1, category: '서울시 종로구' },
+  { id: 2, category: '서울시 중구' },
+  { id: 3, category: '서울시 용산구' },
+  { id: 4, category: '서울시 성동구' },
+  { id: 5, category: '서울시 광진구' },
+  { id: 6, category: '서울시 동대문구' },
+  { id: 7, category: '서울시 중랑구' },
+  { id: 8, category: '서울시 성북구' },
+  { id: 9, category: '서울시 강북구' },
+  { id: 10, category: '서울시 도봉구' },
+  { id: 11, category: '서울시 노원구' },
+  { id: 12, category: '서울시 은평구' },
+  { id: 13, category: '서울시 서대문구' },
+  { id: 14, category: '서울시 마포구' },
+  { id: 15, category: '서울시 양천구' },
+  { id: 16, category: '서울시 강서구' },
+  { id: 17, category: '서울시 구로구' },
+  { id: 18, category: '서울시 금천구' },
+  { id: 19, category: '서울시 영등포구' },
+  { id: 20, category: '서울시 동작구' },
+  { id: 21, category: '서울시 관악구' },
+  { id: 22, category: '서울시 서초구' },
+  { id: 23, category: '서울시 강남구' },
+  { id: 24, category: '서울시 송파구' },
+  { id: 25, category: '서울시 강동구' },
+];
 export default function ProfileEditForm() {
   const {
     register,
@@ -33,14 +60,18 @@ export default function ProfileEditForm() {
               required: '연락처를 입력해주세요',
               pattern: {
                 value: /^(01[0-9])[0-9]{4}[0-9]{4}$/,
-                message: '연락처 형식을 적어주세요',
+                message: '연락처 형식으로 적어주세요. ex)010********',
               },
             })}
           />
           <Controller
             name="address"
             control={control}
-            render={({ field: { onChange } }) => <SelectInput onChange={onChange}>선호 지역</SelectInput>}
+            render={({ field: { onChange } }) => (
+              <SelectInput onChange={onChange} renderList={ADDRESS_LIST}>
+                선호 지역
+              </SelectInput>
+            )}
           />
         </div>
         <div className="flex flex-col gap-2">
