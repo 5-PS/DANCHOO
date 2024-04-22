41aa499a347b52c98ada6f4ed4a371e0304770c3
SONAMJU <skawn1228@gmail.com>
2024-04-22 21:37:04 +0900

    refactor: prop 타입 수정
    - 타입 범위 더 좁게 수정

--- app/(auth)/signup/_components/memberType.tsx
+++ app/(auth)/signup/_components/memberType.tsx
@@ -5,18 +5,18 @@ import Image from 'next/image';
 import checkedIcon from '@/public/icons/checked.svg';
 import uncheckedIcon from '@/public/icons/unchecked.svg';
 
-const MEMBER_TYPE = [
+const MEMBER_TYPE: { label: string; type: 'employee' | 'employer' }[] = [
   { label: '알바님', type: 'employee' },
   { label: '사장님', type: 'employer' },
 ];
 
 interface MemberTypeProps {
-  value: string;
-  onChange: (value: string) => void;
+  value: (typeof MEMBER_TYPE)[number]['type'];
+  onChange: (value: 'employee' | 'employer') => void;
 }
 
 export default function MemberType({ value, onChange }: MemberTypeProps) {
-  const getButtonClassName = (buttonValue: string) => {
+  const getButtonClassName = (buttonValue: 'employee' | 'employer') => {
     const baseButtonClassName =
       'py-[13px] w-[167px] text-[14px] leading=[22px] rounded-[30px] border border-solid bg-white flex gap-[9px] justify-center items-center';
 
