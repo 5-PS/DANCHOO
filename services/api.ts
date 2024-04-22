4fe3c4b285292d86ec5f6c1b3d56bee1f8ff1f2c
SONAMJU <skawn1228@gmail.com>
2024-04-22 22:41:10 +0900

    refactor: api 함수에 구조분해할당 적용

--- services/api.ts
+++ services/api.ts
@@ -2,11 +2,11 @@ import apiClient from '@/libs/axios';
 import { PostSignInBody, PostSignupBody } from '@/types/api';
 
 export async function postSignUpInfo({ email, password, confirmPassword, type }: PostSignupBody) {
-  const response = await apiClient.post('/users', { email, password, confirmPassword, type });
-  return response.data;
+  const { data } = await apiClient.post('/users', { email, password, confirmPassword, type });
+  return data;
 }
 
 export async function postSignIn({ email, password }: PostSignInBody) {
-  const response = await apiClient.post('/token', { email, password });
-  return response.data;
+  const { data } = await apiClient.post('/token', { email, password });
+  return data;
 }
