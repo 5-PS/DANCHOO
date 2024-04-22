8783effaa38d4da58c3508a39009f44fdfa869e7
minho <kasamin98@naver.com>
2024-04-22 16:17:45 +0900

    feat: url/my-store 페이지 생성

---
+++ app/(service)/my-store/page.tsx
@@ -0,0 +1,23 @@
+import Footer from '@/components/footer/footer';
+import Gnb from '@/components/gnb/gnb';
+
+import TestHaveRegisteredNotice from './_components/haveRegisteredNotice';
+import MyStoreCard from './_components/myStoreCard';
+import RegisteredNotice from './_components/registeredNotice';
+
+/**
+ * @todo 데이터가 있다면 <haveRegisteredNotice />, 없다면 <RegisteredNotice /> 예정
+ */
+export default function MyStore() {
+  return (
+    <>
+      <Gnb />
+      <div className="flex-col items-center justify-center ">
+        <MyStoreCard />
+        <RegisteredNotice />
+        <TestHaveRegisteredNotice />
+      </div>
+      <Footer />
+    </>
+  );
+}
