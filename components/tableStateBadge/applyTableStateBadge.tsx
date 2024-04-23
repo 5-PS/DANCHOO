7d4c133344cb3ed3697888c00076c6bcb9150f8c
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-23 13:49:53 +0900

    design: 뱃지 디자인 수정

--- components/tableStateBadge/applyTableStateBadge.tsx
+++ components/tableStateBadge/applyTableStateBadge.tsx
@@ -1,11 +1,13 @@
 interface ApplyTableStateBadgeProps {
-  state: 'pending' | 'accepted ' | 'rejected';
+  state: 'pending' | 'accepted' | 'rejected';
 }
 
 export default function ApplyTableStateBadge({ state }: ApplyTableStateBadgeProps) {
-  const badgeStyle = `${state === 'accepted ' ? 'bg-blue-10 text-blue-20' : ''}${state === 'pending' ? 'bg-green-10 text-green-20' : ''}${state === 'rejected' ? 'bg-red-200 text-red-500' : ''}`;
-  const badgeText = `${state === 'accepted ' ? '승인 완료' : ''}${state === 'pending' ? '대기중' : ''}${state === 'rejected' ? '거절' : ''}`;
+  const badgeStyle = `${state === 'accepted' ? 'bg-blue-10 text-blue-20' : ''}${state === 'pending' ? 'bg-green-10 text-green-20' : ''}${state === 'rejected' ? 'bg-red-200 text-red-500' : ''}`;
+  const badgeText = `${state === 'accepted' ? '승인 완료' : ''}${state === 'pending' ? '대기중' : ''}${state === 'rejected' ? '거절' : ''}`;
   return (
-    <span className={`px-[10px] py-[6px] text-[12px] md:text-[14px] md:font-bold ${badgeStyle}`}>{badgeText}</span>
+    <span className={`px-[10px] py-[6px] rounded-[20px] text-[12px] md:text-[14px] md:font-bold ${badgeStyle}`}>
+      {badgeText}
+    </span>
   );
 }
