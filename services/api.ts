77841032ed4f8f9a0ab1dd4d919439ddcf683439
영은\Hi <popduddms@gmail.com>
2024-04-24 15:31:07 +0900

    feat: 맞춤공고랑 전체공고 분리 추후 수정예정

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
 import apiClient from '@/libs/axios';
-import { PostSignInBody, PostSignupBody } from '@/types/api';
+import { GetNoticesParams, PostSignInBody, PostSignupBody } from '@/types/api';
 
 export async function postSignUpInfo({ email, password, confirmPassword, type }: PostSignupBody) {
   const { data } = await apiClient.post('/users', { email, password, confirmPassword, type });
@@ -11,7 +11,30 @@ export async function postSignIn({ email, password }: PostSignInBody) {
   return data;
 }
 
-export const getNotices = async () => {
+export const getNotices = async ({
+  offset = 0,
+  limit = 6,
+  address,
+  keyword,
+  startsAtGte,
+  hourlyPayGte,
+  sort,
+}: GetNoticesParams) => {
+  const { data } = await apiClient.get('/notices', {
+    params: {
+      offset,
+      limit,
+      address,
+      keyword,
+      startsAtGte,
+      hourlyPayGte,
+      sort,
+    },
+  });
+  return data;
+};
+
+export const getPersonalNotices = async () => {
   const { data } = await apiClient.get('/notices');
   return data;
 };
