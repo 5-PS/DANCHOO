0cdbf3b9657369df7f738b82fa4d3a30ab307b3f
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-24 22:28:47 +0900

    fix: 페이지 태그 수정

--- app/(service)/my-store/[store-Id]/my-recruits/[recruitId]/page.tsx
+++ app/(service)/my-store/[store-Id]/my-recruits/[recruitId]/page.tsx
@@ -4,17 +4,14 @@ import RecuitsCard from './_components/recruitsCard';
 
 interface MyRecruitDetailPageProps {
   searchParams: { page: string };
+  params: any;
 }
 
-function MyRecruitDetailPage({ searchParams }: MyRecruitDetailPageProps) {
+function MyRecruitDetailPage({ params, searchParams }: MyRecruitDetailPageProps) {
   return (
     <main>
       <section className="my-recuits-detail-section">
-        <div className="mb-4 md:mb-6">
-          <span className="text-[14px] inline-block mb-2 font-bold text-primary md:text-[16px]">식당</span>
-          <h1 className="text-[20px] font-bold md:text-[28px]">도토리 식당</h1>
-        </div>
-        <RecuitsCard />
+        <RecuitsCard storeId={params['store-Id']} recruitId={params.recruitId} />
       </section>
       <section className="my-recuits-detail-section">
         <h1 className="text-[20px] font-bold mb-4 md:text-[28px] md:mb-8 ">신청자 목록</h1>
