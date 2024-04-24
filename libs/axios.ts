f8cf62de5da4929967f9d8efef965eae6f9a20b8
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-24 22:26:25 +0900

    fix: axios 인스턴스 버그 잠시 보류

--- libs/axios.ts
+++ libs/axios.ts
@@ -9,10 +9,10 @@ const apiClient = axios.create({
 
 export default apiClient;
 
-export const postRequest = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${document.cookie.split('=')[1]}`,
-  },
-});
+// export const postRequest = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_BASE_API_URL,
+//   headers: {
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${document?.cookie.split('=')[1]}`,
+//   },
+// });
