83ba151eac01935710e7d0edf73418044dbb8b0c
minho <kasamin98@naver.com>
2024-04-24 18:02:49 +0900

    refactor: 내 가게 페이지 파일 분리(등록된 내 가게가 있을 때와 없을 때)

--- app/(service)/my-store/page.tsx
+++ app/(service)/my-store/page.tsx
@@ -1,8 +1,6 @@
 import Footer from '@/components/footer/footer';
 
-import TestHaveRegisteredNotice from './[store-Id]/_components/haveRegisteredNotice';
-import MyStoreCard from './[store-Id]/_components/myStoreCard';
-import RegisteredNotice from './[store-Id]/_components/registeredNotice';
+import EmptyNotice from './_components/emptyStore';
 
 /**
  * @todo 데이터가 있다면 <haveRegisteredNotice />, 없다면 <RegisteredNotice /> 예정
@@ -11,14 +9,7 @@ export default function MyStore() {
   return (
     <>
       <div className="flex-col items-center justify-center ">
-        <div className="mt-10 ">
-          <h2 className="text-black text-[28px] font-bold mb-6 w-[351px] xl:w-[964px] mx-auto md:w-[632px] border-gray-20 md:mb-6">
-            내 가게
-          </h2>
-          <MyStoreCard />
-        </div>
-        <RegisteredNotice />
-        <TestHaveRegisteredNotice />
+        <EmptyNotice />
       </div>
       <Footer />
     </>
