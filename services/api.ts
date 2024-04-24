f379e57c644a3b3a4d0b0b21996571a0f4188da9
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-24 15:20:19 +0900

    feat: 내 프로필 생성 api 생성

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,5 @@
-import apiClient from '@/libs/axios';
-import { PostSignInBody, PostSignupBody } from '@/types/api';
+import apiClient, { postRequest } from '@/libs/axios';
+import { PostSignInBody, PostSignupBody, PutProfileBody } from '@/types/api';
 
 export async function postSignUpInfo({ email, password, confirmPassword, type }: PostSignupBody) {
   const { data } = await apiClient.post('/users', { email, password, confirmPassword, type });
@@ -10,3 +10,8 @@ export async function postSignIn({ email, password }: PostSignInBody) {
   const { data } = await apiClient.post('/token', { email, password });
   return data;
 }
+
+export const putUserProfile = async (userId: string | string[], formData: PutProfileBody) => {
+  const { data } = await postRequest.put(`users/${userId}`, formData);
+  return data;
+};
