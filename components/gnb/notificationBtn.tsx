02e96290ff15fb4391f3ae6c7d2eb5d878b14f78
황준용 <wnsdyd3333@gmail.com>
2024-04-25 19:36:42 +0900

    feat: 안 읽은 알람만 뜨게 구현

--- components/gnb/notificationBtn.tsx
+++ components/gnb/notificationBtn.tsx
@@ -6,45 +6,27 @@ import { AxiosError } from 'axios';
 import Image from 'next/image';
 
 import { postRequest } from '@/libs/axios';
+import { AlertItem } from '@/types/notification';
 import jwtDecode from '@/utils/jwtDecode';
 
 import NotificationBoard from '../notificationModal/notificationBoard';
 
-interface AlertItem {
-  item: {
-    id: string;
-    createdAt: string;
-    result: 'accepted' | 'rejected';
-    read: boolean;
-    shop: {
-      item: {
-        id: string;
-        name: string;
-      };
-    };
-    notice: {
-      item: {
-        startsAt: string;
-        workhour: number;
-      };
-    };
-  };
-}
-interface NotificationBoardProps {
+interface AlertList {
   count: number;
   items: AlertItem[];
 }
 
 function NotificationBtn() {
   const [isNotificationOpen, setIsNotificationOpen] = useState(false);
-  const [alertList, setAlertList] = useState<NotificationBoardProps>({ count: 0, items: [] });
+  const [alertList, setAlertList] = useState<AlertList>({ count: 0, items: [] });
 
   useEffect(() => {
     const userId = jwtDecode();
     const fetchData = async () => {
       try {
         const { data } = await postRequest.get(`/users/${userId}/alerts`);
-        setAlertList(data);
+        const unreadAlerts = data.items.filter((item: { item: { read: boolean } }) => !item.item.read);
+        setAlertList({ count: unreadAlerts.length, items: unreadAlerts });
       } catch (error) {
         if (error instanceof AxiosError) {
           const errorMessage = error.response?.data.message;
@@ -57,10 +39,10 @@ function NotificationBtn() {
   return (
     <>
       <button onClick={() => setIsNotificationOpen((prev) => !prev)}>
-        {true ? (
-          <Image src="/icons/notification-inactive.svg" width={20} height={20} alt="알림 아이콘" />
-        ) : (
+        {alertList.count ? (
           <Image src="/icons/notification-active.svg" width={20} height={20} alt="알림 아이콘" />
+        ) : (
+          <Image src="/icons/notification-inactive.svg" width={20} height={20} alt="알림 아이콘" />
         )}
       </button>
       {isNotificationOpen && (
