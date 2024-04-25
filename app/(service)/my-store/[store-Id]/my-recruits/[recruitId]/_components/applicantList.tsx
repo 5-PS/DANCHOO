bb7450c38069bf24d8a1d8f598d2ef7ca87fca5a
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-25 17:17:46 +0900

    design: 신청자가 없을 때 보여주는 컴포넌트 디자인 변경

--- app/(service)/my-store/[store-Id]/my-recruits/[recruitId]/_components/applicantList.tsx
+++ app/(service)/my-store/[store-Id]/my-recruits/[recruitId]/_components/applicantList.tsx
@@ -9,9 +9,12 @@ interface ApplicantListProps {
 export default async function ApplicantList({ searchParams, storeId, recruitId }: ApplicantListProps) {
   const offset = searchParams.page ? Number(searchParams.page) : 1;
   const response = await getRecruitApplyList(storeId, recruitId, offset);
-  console.log(offset);
   if (response.count === 0) {
-    return <div>아직 신청자가 없어요!</div>;
+    return (
+      <div className="flex items-center justify-center w-full min-h-[200px] border rounded-2xl border-gray-30 text-[24px] text-gray-30 md:min-h-[450px] md:text-[32px]">
+        아직 신청자가 없어요!
+      </div>
+    );
   }
   const { items } = response;
 
