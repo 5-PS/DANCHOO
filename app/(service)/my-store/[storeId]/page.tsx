2f710768baa777277fe68d03ad5c6b559683d60b
minho <kasamin98@naver.com>
2024-04-25 19:13:35 +0900

    fix: 파일이름 컨벤션에 맞게 변경

--- app/(service)/my-store/[storeId]/page.tsx
+++ app/(service)/my-store/[storeId]/page.tsx
@@ -1,14 +1,17 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+
 import Footer from '@/components/footer/footer';
 
 import MyStoreCard from './_components/myStoreCard';
 import RegisteredNotice from './_components/registeredNotice';
 
-/**
- * @todo 데이터가 있다면 <haveRegisteredNotice />, 없다면 <RegisteredNotice /> 예정
- */
 export default function MyStore() {
+  const params = useParams();
   return (
     <>
+      {console.log(params)}
       <div className="flex-col items-center justify-center ">
         <div className="mt-10 ">
           <h2 className="text-black text-[28px] font-bold mb-6 w-[351px] xl:w-[964px] mx-auto md:w-[632px] border-gray-20 md:mb-6">
