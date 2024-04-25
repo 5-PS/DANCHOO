dbb7c6bf516fe6e229460d13ae82ed56a1ec8fc9
minho <kasamin98@naver.com>
2024-04-25 14:24:43 +0900

    feat: my-store 위치 올바르게 수정 및 페이지에 맞는 화면 구성

--- app/(service)/my-store/page.tsx
+++ app/(service)/my-store/page.tsx
@@ -2,9 +2,6 @@ import Footer from '@/components/footer/footer';
 
 import EmptyNotice from './_components/emptyStore';
 
-/**
- * @todo 데이터가 있다면 <haveRegisteredNotice />, 없다면 <RegisteredNotice /> 예정
- */
 export default function MyStore() {
   return (
     <>
