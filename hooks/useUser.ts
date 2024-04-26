b74ee732e1eba92d5158e8a2cb9486a5b40241d7
SONAMJU <skawn1228@gmail.com>
2024-04-26 13:24:55 +0900

    feat: `useUser` custom hook 생성
    - 유저 정보를 받아오는 훅 생성

---
+++ hooks/useUser.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+
+import { getUserProfile } from '@/services/api';
+import decodeJWT from '@/utils/decodeJWT';
+
+const useUser = () => {
+  const [user, setUser] = useState('');
+
+  const token = document.cookie;
+  const tokenValue = token.split('=')[1];
+
+  const decodedPayload = decodeJWT(tokenValue);
+
+  const getUser = async () => {
+    const { item } = await getUserProfile(decodedPayload.userId);
+    setUser(item);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return user;
+};
+
+export default useUser;
