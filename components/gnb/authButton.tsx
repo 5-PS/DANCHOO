682f7d3e1de1405a5cf69a3d65aafb99dd60d578
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-26 22:22:47 +0900

    feat: gnb 기능 구현

--- components/gnb/authButton.tsx
+++ components/gnb/authButton.tsx
@@ -2,68 +2,70 @@
 
 import { useEffect, useState } from 'react';
 
-import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-import { useToastContext } from '@/contexts/toastContext';
-import useCookieToken from '@/hooks/useCookieToken';
-import apiClient from '@/libs/axios';
-import jwtDecode from '@/utils/decodeJWT';
+import { apiClient } from '@/libs/axios';
+import { decodeJWT, getCookie } from '@/utils/getCookie';
 
 import NotificationBtn from './notificationBtn';
 
-interface UserTypeProps {
-  userType?: string;
+interface BoxProps {
+  userId?: string;
+  type?: string;
+  shopId?: string;
 }
 
-function UserTypeLink({ userType }: UserTypeProps) {
-  return userType === 'employee' ? <Link href="/my-profile">내 프로필</Link> : <Link href="/my-store">내 가게</Link>;
+function UserTypeLink({ userType }: { userType: BoxProps }) {
+  return userType.type === 'employer' ? (
+    <Link href={`${userType?.shopId ? `/my-store/${userType.shopId}` : `/my-store`}`}>내가게</Link>
+  ) : (
+    <Link href="/my-profile">내 프로필</Link>
+  );
+}
+
+const getUser = async (userId: any) => apiClient.get(`/users/${userId}`);
+
+function UserContentBox({ userId, logout }: { userId?: string; logout: any }) {
+  const { data } = useQuery({ queryKey: ['userInfo1'], queryFn: () => getUser(userId) });
+
+  const userInfo: BoxProps = {
+    userId: data?.data.item.id,
+    type: data?.data.item.type,
+    shopId: data?.data.item.shop?.item.id,
+  };
+
+  return (
+    <>
+      <UserTypeLink userType={userInfo} />
+      <button onClick={logout} type="button">
+        로그아웃
+      </button>
+      <NotificationBtn />
+    </>
+  );
 }
 
 function AuthButton() {
   const router = useRouter();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userType, setUserType] = useState<string>();
-  const { showToast } = useToastContext();
-  const token = useCookieToken();
-
-  useEffect(() => {
-    setIsLoggedIn(!!token);
-
-    if (token) {
-      const tokenPayload = document.cookie.split('.')[1];
-      const userId = jwtDecode(tokenPayload);
-      const fetchData = async () => {
-        try {
-          const { data } = await apiClient.get(`/users/${userId}`);
-          setUserType(data.item.type);
-        } catch (error) {
-          if (error instanceof AxiosError) {
-            const errorMessage = error.response?.data.message;
-            alert(errorMessage);
-          }
-        }
-      };
-      fetchData();
-    }
-  }, [token]);
-
+  const [token, setToken] = useState<string | undefined>('');
+  const userId = token && decodeJWT(token);
   const handleLogout = () => {
-    document.cookie = 'accessToken=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-    setIsLoggedIn(false);
-    showToast('로그아웃 되었습니다.');
+    document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    setToken('');
+    alert('로그아웃 되었습니다');
     router.push('/');
   };
+  useEffect(() => {
+    const accessToken = getCookie('accessToken');
+    setToken(accessToken);
+  }, []);
 
   return (
     <div className="flex items-center gap-5 ml-auto font-bold text-black md:order-1">
-      {isLoggedIn ? (
-        <>
-          <UserTypeLink userType={userType} />
-          <button onClick={handleLogout}>로그아웃</button>
-          <NotificationBtn />
-        </>
+      {token ? (
+        <UserContentBox userId={userId} logout={handleLogout} />
       ) : (
         <>
           <Link href="/signin">로그인</Link>
@@ -73,4 +75,5 @@ function AuthButton() {
     </div>
   );
 }
+
 export default AuthButton;
--- utils/getCookie.ts
+++ utils/getCookie.ts
@@ -1,7 +1,13 @@
-export default function getCookie(name: string) {
+export function getCookie(name: string) {
   const matches = document.cookie.match(
     // eslint-disable-next-line no-useless-escape
     new RegExp(`(?:^|; )${name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1')}=([^;]*)`),
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+export function decodeJWT(token: string) {
+  const base64payload = token.split('.')[1];
+  const payload = Buffer.from(base64payload, 'base64');
+  const result = JSON.parse(payload.toString());
+  return result.userId;
 }
