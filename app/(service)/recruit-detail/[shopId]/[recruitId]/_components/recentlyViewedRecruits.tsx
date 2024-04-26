b64a00c15e8fac95af8079473bd4dea2a844fee8
황준용 <wnsdyd3333@gmail.com>
2024-04-27 00:07:16 +0900

    feat: 타입 지정

--- app/(service)/recruit-detail/[shopId]/[recruitId]/_components/recentlyViewedRecruits.tsx
+++ app/(service)/recruit-detail/[shopId]/[recruitId]/_components/recentlyViewedRecruits.tsx
@@ -4,13 +4,27 @@ import { useEffect, useState } from 'react';
 
 import Post from '@/components/post/post';
 
+interface RecentlyViewedRecruitsItem {
+  id: string;
+  shopId: string;
+  address: string;
+  imageUrl: string;
+  name: string;
+  hourlyPay: number;
+  originalHourlyPay: number;
+  startsAt: string;
+  workhour: number;
+  closed: boolean;
+}
+
 function RecentlyViewedRecruits() {
-  const [recentlyViewedRecruits, setRecentlyViewedRecruits] = useState([]);
+  const [recentlyViewedRecruits, setRecentlyViewedRecruits] = useState<RecentlyViewedRecruitsItem[]>();
 
   useEffect(() => {
     const storedRecruits = JSON.parse(sessionStorage.getItem('recentlyViewedRecruits') || '[]');
     setRecentlyViewedRecruits(storedRecruits);
   }, []);
+
   return (
     <ul className="grid grid-cols-2 gap-x-2 gap-y-4 md:gap-x-[14px] md:gap-y-8 xl:grid-cols-3">
       {recentlyViewedRecruits?.map((list) => (
