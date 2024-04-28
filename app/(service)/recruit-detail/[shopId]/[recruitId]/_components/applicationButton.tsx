d70777dba5860609a0856309eb26b0a01dedd7ae
황준용 <wnsdyd3333@gmail.com>
2024-04-28 10:21:54 +0900

    feat: 커스텀 훅 사용해서 코드 수정

--- app/(service)/recruit-detail/[shopId]/[recruitId]/_components/applicationButton.tsx
+++ app/(service)/recruit-detail/[shopId]/[recruitId]/_components/applicationButton.tsx
@@ -1,13 +1,8 @@
 'use client';
 
 import { useState } from 'react';
-
-import { AxiosError } from 'axios';
-import { useRouter } from 'next/navigation';
-
 import Button from '@/components/button/button';
-import { useToastContext } from '@/contexts/toastContext';
-import { postRequest } from '@/libs/axios';
+import useRecruitActions from '@/hooks/useRecruitActions';
 
 interface ApplicationButtonProps {
   isClosed: boolean;
@@ -16,53 +11,32 @@ interface ApplicationButtonProps {
 }
 
 function ApplicationButton({ isClosed, shopId, recruitId }: ApplicationButtonProps) {
-  const [isApplied, setIsApplied] = useState(false);
-  const [applicationsId, setApplicationsId] = useState('');
-  const router = useRouter();
-  const { showToast } = useToastContext();
-  const handleRecruitApplication = async () => {
-    try {
-      const { data } = await postRequest.post(`/shops/${shopId}/notices/${recruitId}/applications`);
-      setApplicationsId(data.item.id);
-      showToast('신청 완료!');
-      setIsApplied(true);
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 400) alert('공고가 마감되었습니다.');
-        if (error.response?.status === 401) {
-          const result = confirm('로그인이 필요합니다');
-          if (result) {
-            router.push('/signin');
-          }
-        }
-        if (error.response?.status === 404) alert('존재하지 않는 공고입니다.');
-      }
-    }
-  };
 
-  const handleCancelApplication = async () => {
-    try {
-      const result = confirm('정말 취소하실건가요?');
-      if (result) {
-        await postRequest.put(`/shops/${shopId}/notices/${recruitId}/applications/${applicationsId}`, {
-          status: 'canceled',
-        });
-        showToast('취소했어요');
-        setIsApplied(false);
-      }
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 403) alert('공고 지원자만 접근할 수 있습니다');
-        if (error.response?.status === 404) alert('존재하지 않는 공고입니다.');
+  const [isApplied, setIsApplied] = useState(false);
+  const { applyRecruit, cancelRecruit, type } = useRecruitActions(shopId, recruitId)
+
+  const handleRecruitActions = () => {
+    if (type === 'employee') {
+      if (!isApplied) {
+        applyRecruit()
+        setIsApplied(true)
+      } else {
+        const result = confirm('신청을 취소하시겠습니까?')
+        if (result){
+          cancelRecruit()
+          setIsApplied(false)
+        }
       }
     }
-  };
+    if (type === 'employer') alert('사장님은 신청못합니다!')
+
+  }
 
   return (
     <Button
       background={`${isClosed ? 'bg-gray-40' : isApplied ? 'bg-white' : 'bg-primary'}`}
       className="h-[48px] font-bold"
-      onClick={isApplied ? handleCancelApplication : handleRecruitApplication}
+      onClick={handleRecruitActions}
     >
       {isClosed ? '신청 불가' : isApplied ? '신청 취소' : '신청 하기'}
     </Button>
