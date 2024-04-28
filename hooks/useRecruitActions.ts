1e0dd1962bbe752caba9f4f4387a6bb358542a4b
황준용 <wnsdyd3333@gmail.com>
2024-04-28 10:21:37 +0900

    feat: 공고 신청 및 취소 커스텀 훅 생성

---
+++ hooks/useRecruitActions.ts
@@ -0,0 +1,58 @@
+import { useToastContext } from '@/contexts/toastContext';
+import { postApplyRecruit, putCancelRecruit } from '@/services/api';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+interface UserType {
+  data: {
+    item: {
+      type: string;
+    };
+  };
+}
+const useRecruitActions = (shopId: string, recruitId: string) => {
+  const [applicationsId, setApplicationsId] = useState('');
+  const { showToast } = useToastContext();
+  const router = useRouter();
+  const { data } = useQuery<UserType>({ queryKey: ['userInfo1'] });
+  const type = data?.data.item.type;
+
+  const { mutate: applyRecruit } = useMutation({
+    mutationFn: () => postApplyRecruit(shopId, recruitId),
+    onSuccess: (data: { item: { id: string } }) => {
+      setApplicationsId(data.item.id);
+      showToast('신청 완료!');
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 400) alert('공고가 마감되었습니다.');
+        if (error.response?.status === 401) {
+          const result = confirm('로그인이 필요합니다');
+          if (result) {
+            router.push('/signin');
+          }
+        }
+        if (error.response?.status === 404) alert('존재하지 않는 공고입니다.');
+      }
+    },
+  });
+
+  const { mutate: cancelRecruit } = useMutation({
+    mutationFn: () => putCancelRecruit(shopId, recruitId, applicationsId),
+    onSuccess: () => {
+      showToast('취소했어요');
+    },
+    onError: (error) => {
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 403) alert('공고 지원자만 접근할 수 있습니다');
+        if (error.response?.status === 404) alert('존재하지 않는 공고입니다.');
+      }
+    },
+  });
+
+  return { applyRecruit, cancelRecruit, type };
+};
+
+export default useRecruitActions;
