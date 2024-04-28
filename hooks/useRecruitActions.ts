dda3b85d34bd7502b2d4703b09edc2e9b71b86d9
황준용 <wnsdyd3333@gmail.com>
2024-04-28 20:13:43 +0900

    feat: 에러코드 분리

--- hooks/useRecruitActions.ts
+++ hooks/useRecruitActions.ts
@@ -2,6 +2,7 @@ import { useToastContext } from '@/contexts/toastContext';
 import { getDetailRecruit, getUserApplyList, postApplyRecruit, putCancelRecruit } from '@/services/api';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
@@ -42,6 +43,30 @@ const useRecruitActions = (shopId: string, recruitId: string) => {
   });
   const user = data?.data.item;
 
+  const handleAxiosError = (error: AxiosError, router: AppRouterInstance, showToast: (_msg: string) => void) => {
+    if (error instanceof AxiosError) {
+      switch (error.response?.status) {
+        case 400:
+          alert('공고가 마감되었습니다.');
+          break;
+        case 401:
+          const result = confirm('로그인이 필요합니다');
+          if (result) {
+            router.push('/signin');
+          }
+          break;
+        case 403:
+          alert('공고 지원자만 접근할 수 있습니다');
+          break;
+        case 404:
+          alert('존재하지 않는 공고입니다.');
+          break;
+        default:
+          showToast('오류가 발생했습니다.');
+      }
+    }
+  };
+
   useEffect(() => {
     if (status) {
       const applyRecruitId = status?.items.filter((state) => state.item.notice.item.id === recruitId);
@@ -57,18 +82,7 @@ const useRecruitActions = (shopId: string, recruitId: string) => {
       showToast('신청 완료!');
       refetch();
     },
-    onError: (error) => {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 400) alert('공고가 마감되었습니다.');
-        if (error.response?.status === 401) {
-          const result = confirm('로그인이 필요합니다');
-          if (result) {
-            router.push('/signin');
-          }
-        }
-        if (error.response?.status === 404) alert('존재하지 않는 공고입니다.');
-      }
-    },
+    onError: (error: AxiosError) => handleAxiosError(error, router, showToast),
   });
 
   const { mutate: cancelRecruit } = useMutation({
@@ -76,12 +90,7 @@ const useRecruitActions = (shopId: string, recruitId: string) => {
     onSuccess: () => {
       showToast('취소했어요');
     },
-    onError: (error) => {
-      if (error instanceof AxiosError) {
-        if (error.response?.status === 403) alert('공고 지원자만 접근할 수 있습니다');
-        if (error.response?.status === 404) alert('존재하지 않는 공고입니다.');
-      }
-    },
+    onError: (error: AxiosError) => handleAxiosError(error, router, showToast),
   });
 
   return { applyRecruit, cancelRecruit, user, status };
