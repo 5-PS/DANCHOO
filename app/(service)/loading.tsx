1222632fa4cacfdde6d07dab1dbff5495bc42a8f
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-28 14:08:25 +0900

    feat: loading UI 추가

--- app/(service)/loading.tsx
+++ app/(service)/loading.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default function Loading () {
   return(
-    <div>Loading</div>
+    <div className="absolute top-0 left-0 flex items-center justify-center w-full h-screen">
+    <Image src='/icons/loading.svg' width={200} height={200} alt="로딩 UI"/>
+</div>
   )
 }
