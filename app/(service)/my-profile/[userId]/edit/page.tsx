07edc085c57107a57bc9470027a3c5b2ac6f9df6
parkhyeonwoo <pq120wal39@gmail.com>
2024-04-29 22:16:24 +0900

    fix: 프로필 수정 폼 모달 적용

--- app/(service)/my-profile/[userId]/edit/page.tsx
+++ app/(service)/my-profile/[userId]/edit/page.tsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 
 import ProfileEditForm from './_components/profileEditForm';
 
-export default function MyProfileEditPage() {
+export default function MyProfileEditPage({params} : {params : {userId : string | string[]}}) {
   return (
     <div className="w-full px-3 pt-10 pb-20 md:py-[60px] md:max-w-[680px] md:m-auto xl:max-w-[964px]">
       <div className="flex items-center justify-between mb-6 md:mb-8 ">
         <h1 className="text-xl font-bold md:text-[28px]">내 프로필</h1>
-        <Link href="/my-profile" className="relative w-6 h-6 md:w-8 md:h-8">
+        <Link href={`/my-profile/${params.userId}`} className="relative w-6 h-6 md:w-8 md:h-8">
           <Image src="/icons/close-modal-icon.svg" fill alt="이전버튼" />
         </Link>
       </div>
